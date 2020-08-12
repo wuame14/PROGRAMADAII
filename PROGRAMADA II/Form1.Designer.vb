3b7119f18baecaf187f3d631a5acf3c4d40b8efd
ESTEBAN QUIROS CERDAS DANIEL <danielquiros@estudiantec.cr>
2020-08-12 10:26:16 -0600

    hice boton 5

--- PROGRAMADA II/Form1.Designer.vb
+++ PROGRAMADA II/Form1.Designer.vb
@@ -27,64 +27,81 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(293, 9)
+        Me.Label1.Location = New System.Drawing.Point(440, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(119, 39)
+        Me.Label1.Size = New System.Drawing.Size(182, 61)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "MENÚ"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(116, 97)
+        Me.Button1.Location = New System.Drawing.Point(174, 149)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(111, 23)
+        Me.Button1.Size = New System.Drawing.Size(166, 35)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Tabla Estudiantes"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(116, 145)
+        Me.Button2.Location = New System.Drawing.Point(174, 223)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.Size = New System.Drawing.Size(166, 35)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Tabla Profesores"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(116, 190)
+        Me.Button3.Location = New System.Drawing.Point(174, 292)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(138, 23)
+        Me.Button3.Size = New System.Drawing.Size(207, 35)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "Formulario Estudiantes"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(116, 234)
+        Me.Button4.Location = New System.Drawing.Point(174, 360)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(126, 23)
+        Me.Button4.Size = New System.Drawing.Size(189, 35)
         Me.Button4.TabIndex = 4
         Me.Button4.Text = "Formulario Profesores"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(768, 158)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(127, 52)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
+        Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -97,4 +114,5 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
--- PROGRAMADA II/Form1.vb
+++ PROGRAMADA II/Form1.vb
@@ -41,4 +41,8 @@ Public Class Form1
         F_Profesores.Show()
         Me.Hide()
     End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
 End Class
